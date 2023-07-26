9edef674af96bf764a38db9c68bd1a8fdf1360dc
Maxim <maxim.leonet@gmail.com>
2023-07-26 13:44:23 +0200

    Ended

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mleonet <mleonet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 14:40:14 by mleonet           #+#    #+#             */
-/*   Updated: 2023/07/21 17:56:20 by mleonet          ###   ########.fr       */
+/*   Updated: 2023/07/26 13:44:03 by mleonet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,11 @@ char	*get_next_line(int fd);
 char	*read_and_insert_stash(int fd, char *stash);
 int		is_newline(char *str);
 char	*add_to_stash(char *stash, char *buf);
+char	*extract_line(char **stash);
+char	*extract_line_utils(char **stash, int index, int add);
 size_t	ft_strlen(char *str);
 size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
-char	*extract_line(char **stash);
 char	*get_the_line(char *stash, int line_length, int add);
 char	*get_remainder(char *stash, int line_length);
-char	*extract_line_utils(char **stash, int index, int add);
 
 #endif
